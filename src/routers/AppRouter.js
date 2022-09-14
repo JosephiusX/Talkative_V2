f8e9a7da9cd2cbc41a8bc82b0e27b2218e37206f
josephius x <josephiusx@gmail.com>
2022-09-14 16:48:21 -0700

    working on Notes components

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import TopicDashboardPage from '../components/Topic/TopicDashboardPage';
+import TopicsPage from '../components/Pages/TopicsPage';
 import AddTopicPage from '../components/Pages/AddTopicPage';
 import EditTopicPage from '../components/Pages/EditTopicPage';
 import NotesPage from '../components/Pages/NotesPage';
@@ -12,7 +12,7 @@ const AppRouter = () => (
     <div>
       <Header />
       <Switch>
-        <Route path="/" component={TopicDashboardPage} exact={true} />
+        <Route path="/" component={TopicsPage} exact={true} />
         <Route path="/create" component={AddTopicPage} />
         <Route path="/edit/:id" component={EditTopicPage} />
         <Route path="/notes" component={NotesPage} />
