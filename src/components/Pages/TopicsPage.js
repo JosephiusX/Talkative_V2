1b02f0d3cffd951d24d0314eea3c0408b5174466
josephius x <josephiusx@gmail.com>
2022-09-14 16:48:45 -0700

    working on Notes components

---
+++ src/components/Pages/TopicsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TopicList from '../Topic/TopicList';
+import TopicListFilters from '../Topic/TopicListFilters';
+
+const TopicsPage = () => (
+  <div>
+    <TopicListFilters />
+    <TopicList />
+  </div>
+);
+
+export default TopicsPage;
+   
