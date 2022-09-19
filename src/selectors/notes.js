fc484a31b1a73cdee7d9745ee2a22848164d894f
josephius x <josephiusx@gmail.com>
2022-09-18 20:40:38 -0700

    notes list showing (no notes yet

---
+++ src/selectors/notes.js
@@ -0,0 +1,20 @@
+import moment from 'moment';
+
+// Get visible notes
+
+export default (notes, { text, sortBy, startDate, endDate }) => {
+  return notes.filter((note) => {
+    const createdAtMoment = moment(note.createdAt);
+    const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+    const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+    const textMatch = note.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1;
+    }
+  });
+};
